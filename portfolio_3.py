0c2033556abc9c5524aef6cf3716c17184f3b77c
Daanish8D <146092506+Daanish8D@users.noreply.github.com>
2023-10-10 16:07:23 +0100

    Add files via upload

---
+++ portfolio_3.py
@@ -0,0 +1,42 @@
+name = input('Enter your name: ')
+
+if name:
+    print('Hello', name)
+else:
+    print('Hello Stranger!')
+
+bad_passwords = ['password', "letmein", "sesame", "hello", "justinbieber"]
+
+passwd = input('Please enter a password: ')
+
+if passwd in bad_passwords:
+    print("Common password used")
+
+while True:
+    if len(passwd) < 8:
+        print('Your password is not long enough!')
+        passwd = input('Please enter a password: ')
+    elif len(passwd) > 12:
+        print('Your password is too long!')
+        passwd = input('Please enter a password: ')
+    else:
+        print('Your password is long enough!')
+        break
+passwd2 = input('Confirm your password: ')
+while True:
+    if passwd2 != passwd:
+        print('Passwords do not match.')
+        passwd2 = input('Confirm your password: ')
+    else:
+        print('Congratulations, password set!')
+        break
+
+number = int(input("Enter the number of the multiplication needed: "))
+
+print("The Multiplication Table of: ", number)
+if number < 0:
+    for count in range(-12, 0):
+        print(number, 'x', count, '=', number * count)
+else:
+    for count in range(0, 13):
+        print(number, 'x', count, '=', number * count)
